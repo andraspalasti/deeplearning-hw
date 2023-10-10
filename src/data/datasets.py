cdd941854b7277a6f8a24a5da6303a84c8a14d71
andraspalasti <andraspalasti2@gmail.com>
2023-10-10 20:08:57 +0200

    created dataset to use

---
+++ src/data/datasets.py
@@ -0,0 +1,36 @@
+import csv
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+class AirbusDataset(Dataset):
+    def __init__(self, segmentations_file, imgs_dir):
+        self.img_segmentations: list[Tuple[str, np.ndarray]] = []
+        with open(segmentations_file, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.img_segmentations.append((
+                    row['ImageId'],
+                    np.array(row['EncodedPixels'].split(), dtype=int).reshape((-1, 2))
+                ))
+
+        self.imgs_dir = Path(imgs_dir)
+
+    def __len__(self):
+        return len(self.img_segmentations)
+
+    def __getitem__(self, index: int):
+        img_id, segmentation = self.img_segmentations[index]
+        image = Image.open(self.imgs_dir / img_id)
+        return image, segmentation
+
+
+if __name__ == '__main__':
+    data_dir = Path(__file__).parents[2] / 'data' / 'raw'
+    dataset = AirbusDataset(data_dir / 'train_ship_segmentations_v2.csv', data_dir / 'train_v2')
+    for image, segmentation in dataset:
+        print(image, segmentation)
+        print()
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,4 @@
+import csv
 import zipfile
 from pathlib import Path
 
@@ -28,12 +29,28 @@ def download_dataset(out_path, small=False):
             zip.extractall(out_path)
         file.unlink()
 
+
+def filter_missing(annotations_file, imgs_dir):
+    rows, fieldnames = [], []
+    with open(annotations_file, 'r') as f:
+        reader = csv.DictReader(f)
+        fieldnames = list(reader.fieldnames)
+
+        imgs_dir = Path(imgs_dir)
+        rows = [row for row in reader if (imgs_dir / row['ImageId']).exists()]
+
+    with open(annotations_file, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+
 if __name__ == '__main__':
     import sys
 
-    dataset_size = sys.argv[1] if 2 <= len(sys.argv) else None
-    if dataset_size is None or dataset_size not in ('original', 'small'):
-        print('Usage: python make_dataset.py {original,small}')
+    dataset_size = sys.argv[1] if 2 <= len(sys.argv) else 'small'
+    if dataset_size not in ('original', 'small'):
+        print('Usage: python make_dataset.py [original,small]')
         print('\toriginal: downloads the whole dataset of the competition')
         print('\tsmall: downloads a small dataset of the competition')
         exit(1)
@@ -41,4 +58,8 @@ if __name__ == '__main__':
     out_path = Path(__file__).parents[2] / 'data' / 'raw'
     if not out_path.exists():
         out_path.mkdir(parents=True)
-    download_dataset(out_path, small=True)
+
+    download_dataset(out_path, small=dataset_size == 'small')
+
+    print('Removing unused values from segmentations')
+    filter_missing(out_path / 'train_ship_segmentations_v2.csv', out_path / 'train_v2')
