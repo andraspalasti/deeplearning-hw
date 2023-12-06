71e971cc42cdbf8778b404f49a0874674fc6636f
andraspalasti <andraspalasti2@gmail.com>
2023-12-06 12:46:45 +0100

    argumentifying score generation

--- src/gen_scores.py
+++ src/gen_scores.py
@@ -11,11 +11,22 @@ from src.dice_score import dice_coeff
 from src.data.datasets import AirbusDataset, AirbusRawDataset
 
 
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Evaluate the UNet on test images and target masks')
+    parser.add_argument('--load', '-f', type=str, required=True, help='Load model from a .pth file')
+    parser.add_argument('--out', '-o', type=str, required=True, help='Where to save evaluation results')
+    parser.add_argument('--split', type=bool, default=False, help='Split images into 3x3 equal parts')
+    return parser.parse_args()
+
+
 def main():
+    args = get_args()
+    checkpoint_path = Path(args.load)
+    out_path = Path(args.out)
+
     imgs_dir = Path('data/processed/test')
     segmentations_file = Path('data/processed/test_ship_segmentations.csv')
-    checkpoint_path = Path('checkpoints/checkpoint_epoch5.pth')
-    out_path = Path('evaluation.csv')
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using device: {device}')
@@ -27,13 +38,20 @@ def main():
     model = model.to(device)
     print(f'Loaded model weights from: {checkpoint_path}')
 
-    test_set = AirbusRawDataset(
-        imgs_dir=imgs_dir,
-        segmentations_file=segmentations_file,
-    )
+    if args.split:
+        test_set = AirbusDataset(
+            imgs_dir=imgs_dir,
+            segmentations_file=segmentations_file,
+            should_contain_ship=False
+        )
+    else:
+        test_set = AirbusRawDataset(
+            imgs_dir=imgs_dir,
+            segmentations_file=segmentations_file,
+        )
 
     iou_scores, dice_scores = [], []
-    with tqdm(desc='Evaluating by IoU', total=len(test_set)) as pbar:
+    with tqdm(desc='Evaluating', total=len(test_set)) as pbar:
         running_iou = 0
         running_dice = 0
         for i, (input_img, true_mask) in enumerate(test_set):
@@ -59,6 +77,7 @@ def main():
 
     evaluation = pd.DataFrame({
         'ImageId': test_set.seg_by_img.index.tolist(),
+        'part': [i%9 for i in range(len(test_set))] if args.split else [0] * len(test_set),
         'iou': iou_scores,
         'dice': dice_scores,
     })
